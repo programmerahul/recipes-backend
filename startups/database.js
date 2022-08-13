a552dab307cc03783608ca071af48fb5096594fd
Rahul-Prasad <programmerahul@gmail.com>
2022-08-13 11:07:27 +0530

    heroku env set

--- startups/database.js
+++ startups/database.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
+const config = require("config");
 module.exports = function () {
-  mongoose.connect("mongodb://localhost/Vidly").then(() => {
+  mongoose.connect(config.get("db")).then(() => {
     winston.info("Connected to mongodb...");
   });
 };
