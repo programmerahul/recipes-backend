38cdb3e167cfcb2e62b78b4dcbf4e9e5a35bcc26
Rahul-Prasad <programmerahul@gmail.com>
2022-08-14 16:16:52 +0530

    updated home

--- routes/home.js
+++ routes/home.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 router.get("/", (req, res) => {
   res.render("index", {
-    title: "Movie Genres",
-    message: "To see genres, head over to /api/genres",
+    title: "Movies backend",
+    message: "/api/genres|customer|movies|rentals|users|auth",
   });
 });
 module.exports = router;
